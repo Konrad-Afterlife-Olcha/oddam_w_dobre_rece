bb51c30075b8dec2a9a26d08b316fc5932d8d927
Konrad-Afterlife-Olcha <olchakonrad@gmail.com>
2019-10-16 18:28:10 +0200

    poczatek sekcji z fundacjami

---
+++ src/assets/fundationTexts.js
@@ -0,0 +1,52 @@
+const fundationDatabase = {
+    textAbout: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+    texts: [
+        {
+            header: 'Fundacja "Dbam o zdrowie"',
+            purpose: "Cel i misja: Pomoc osobom znajdującym sie w trudnej sytuacji życiowej.",
+            stuff: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Dla dzieci"',
+            purpose: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+            stuff: "ubrania, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Bez domu"',
+            purpose: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+            stuff: "ubrania, jedzenie, ciepłe koce"
+        },
+        {
+            header: 'Fundacja "Lorem-4"',
+            purpose: "Cel i misja: Lorem ipsum dolor sit amet.",
+            stuff: "ubrania, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Lorem-5"',
+            purpose: "Cel i misja: Lorem ipsum dolor sit amet.",
+            stuff: "ubrania, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Lorem-6"',
+            purpose: "Cel i misja: Lorem ipsum dolor sit amet.",
+            stuff: "ubrania, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Lorem-7"',
+            purpose: "Cel i misja: Lorem ipsum dolor sit amet.",
+            stuff: "ubrania, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Lorem-8"',
+            purpose: "Cel i misja: Lorem ipsum dolor sit amet.",
+            stuff: "ubrania, meble, zabawki"
+        },
+        {
+            header: 'Fundacja "Lorem-9"',
+            purpose: "Cel i misja: Lorem ipsum dolor sit amet.",
+            stuff: "ubrania, meble, zabawki"
+        }
+    ]
+};
+export default fundationDatabase;
+
--- src/components/HomeFoundations.js
+++ src/components/HomeFoundations.js
@@ -1,15 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ReactPaginate from 'react-paginate';
+import fundationDatabase from "../assets/fundationTexts";
+
+
 
 
 class HomeFoundations extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            slide: 1
+        }
+    }
     render() {
 
         return (
             <>
-
+                <div className={"home-foundations-container"}>
+                    <h1 className={"home-foundations-header"}>Komu pomagamy?</h1>
+                    <div className={"home-header-line"}></div>
+                    <nav>
+                        <ul className={"home-foundations-nav"}>
+                            <li onClick={()=>(
+                                this.setState({
+                                    slide: 1
+                                })
+                            )}>Fundacjom</li>
+                            <li onClick={()=>(
+                                this.setState({
+                                    slide: 2
+                                })
+                            )}>Organizacjom pozarządowym</li>
+                            <li onClick={()=>(
+                                this.setState({
+                                    slide: 3
+                                })
+                            )}>Lokalnym zbiórkom </li>
+                        </ul>
+                    </nav>
+                    <Description slideNr={this.state.slide}/>
+                </div>
 
             </>
         )
     }
 }
+class Description extends React.Component {
+    render() {
+        return(
+            <>
+                <div className={"home-foundations-about"}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci neque nihil nobis quisquam vitae. A asperiores cupiditate dicta eligendi ex inventore ipsam itaque laboriosam natus, odio omnis pariatur unde veniam.</p>
+                    {fundationDatabase.texts[1].header}
+                </div>
+
+            </>
+        )
+    }
+}
+
+
+
 export default HomeFoundations;
