7310644878ee31058493ccfaa23be284fa9626ac
Konrad-Afterlife-Olcha <olchakonrad@gmail.com>
2019-10-19 13:05:51 +0200

    zrobiona walidacja logowania i rejestracji

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,38 @@ import {
 
 
 class Login extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            email : "",
+            password: "",
+            emailInvalid: false,
+            passwordInvalid: false,
+            loginSuccess: false
+        }
+    }
+    emailValidation = () => {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(this.state.email).toLowerCase())) {
+            this.setState({
+                emailInvalid: true
+            })
+            console.log("niepoprawny email")
+            return false
+        } else {return true}
+    };
+    passwordValidation = () => {
+        if(this.state.password.length <6){
+            this.setState({
+                passwordInvalid: true
+            })
+            console.log("Hasło za krótkie")
+            return false
+        }
+        else {return true}
+    }
+
+
     render() {
 
         return (
@@ -21,13 +53,39 @@ class Login extends React.Component {
                             <form className={"login-inputs"} action="">
                                 <div>
                                     <p>Email</p>
-                                    <input className={"login-input"} type="email"/>
+                                    <input onChange={(e)=>(
+                                        this.setState({
+                                            email: e.target.value,
+                                            emailInvalid: false
+                                        })
+                                    )} className={"login-input"} type="email" value={this.state.email}/>
+                                    {this.state.emailInvalid ? <p className={"invalid-input"}>Podany email jest nieprawidłowy!</p> : null}
                                 </div>
                                 <div>
                                     <p>Hasło</p>
-                                    <input className={"login-input"} type="password"/>
+                                    <input onChange={(e)=>(
+                                        this.setState({
+                                            password: e.target.value,
+                                            passwordInvalid: false,
+                                        })
+                                    )} className={"login-input"} type="password" value={this.state.password}/>
+                                    {this.state.passwordInvalid ? <p className={"invalid-input"}>Podane hasło jest za krótkie!</p> : null}
                                 </div>
-                                <button className={"button-log-reg log-button"} type="submit">Zaloguj się</button>
+                                <button onClick={(e)=> {
+                                        e.preventDefault();
+                                        this.emailValidation();
+                                        this.passwordValidation();
+                                        if(this.emailValidation() && this.passwordValidation()){
+                                            this.setState({
+                                                loginSuccess: true,
+                                                email : "",
+                                                password: "",
+                                                emailInvalid: false,
+                                                passwordInvalid: false,
+                                            })
+                                            console.log("Pomyślnie zalogowano")
+                                        }
+                                }} className={"button-log-reg log-button"} type="submit">Zaloguj się</button>
                             </form>
                         </div>
                         <button className={"button-log-reg reg-button"}><Link to="/rejestracja">Załóż konto</Link></button>
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,48 @@ import {Link} from "react-router-dom";
 
 
 class Register extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            password: "",
+            password2: "",
+            email: "",
+            invalidPassword: false,
+            invalidPassword2: false,
+            invalidEmail: false
+        }
+    }
+    emailValidation = () => {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(this.state.email).toLowerCase())) {
+            this.setState({
+                invalidEmail: true
+            });
+            console.log("niepoprawny email");
+            return false
+        } else {return true}
+    };
+    passwordValidation = () => {
+        if(this.state.password.length <6){
+            this.setState({
+                invalidPassword: true
+            });
+            console.log("Hasło za krótkie");
+            return false
+        }
+        else {return true}
+    }
+    password2Validation = () => {
+        if(this.state.password !== this.state.password2){
+            this.setState({
+                invalidPassword2: true
+            })
+            console.log("Hasła się różnią")
+            return false
+        }
+        else {return true}
+    }
+
     render() {
 
         return (
@@ -19,17 +61,43 @@ class Register extends React.Component {
 
                                 <div>
                                     <p>Email</p>
-                                    <input className={"login-input"} type="email"/>
+                                    <input className={"login-input"} type="email" onChange={(e)=>(
+                                        this.setState({
+                                            email: e.target.value,
+                                            invalidEmail: false
+                                        })
+                                    )} value={this.state.email}/>
+                                    {this.state.invalidEmail ? <p className={"invalid-input"}>Podany email jest nieprawidłowy!</p> : null}
                                 </div>
                                 <div>
                                     <p>Hasło</p>
-                                    <input className={"login-input"} type="password"/>
+                                    <input className={"login-input"} type="password" onChange={(e)=>(
+                                        this.setState({
+                                            password: e.target.value,
+                                            invalidPassword: false
+                                        })
+                                    )} value={this.state.password}/>
+                                    {this.state.invalidPassword ? <p className={"invalid-input"}>Podane hasło jest za krótkie!</p> : null}
                                 </div>
                                 <div>
                                     <p>Powtórz hasło</p>
-                                    <input className={"login-input"} type="password"/>
+                                    <input className={"login-input"} type="password" onChange={(e)=>(
+                                        this.setState({
+                                            password2: e.target.value,
+                                            invalidPassword2: false
+                                        })
+                                    )} value={this.state.password2}/>
+                                    {this.state.invalidPassword2 ? <p className={"invalid-input"}>Hasła się różnią!</p> : null}
                                 </div>
-                                <button className={"log-button"} type="submit">Załóż konto</button>
+                                <button onClick={(e)=>{
+                                    e.preventDefault();
+                                    this.emailValidation();
+                                    this.passwordValidation();
+                                    this.password2Validation();
+                                    if(this.emailValidation() && this.passwordValidation() && this.password2Validation()){
+                                        console.log("Rejestracja zakończona pomyslnie")
+                                    }
+                                }} className={"log-button"} type="submit">Załóż konto</button>
                             </form>
                         </div>
                         <button className={"reg-button"}><Link to="/logowanie">Zaloguj się</Link></button>
