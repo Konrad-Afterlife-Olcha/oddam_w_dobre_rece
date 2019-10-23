a1e5f9152d6c0a45b8ae9f63b485e5e853efb0af
Konrad-Afterlife-Olcha <olchakonrad@gmail.com>
2019-10-23 19:46:00 +0200

    poczatek sekcji oddaj rzeczy

--- src/components/Donations.js
+++ src/components/Donations.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { store } from 'react-easy-state'
+const donationInfo = store({});
 
 class Donations extends React.Component {
     render() {
@@ -6,18 +8,21 @@ class Donations extends React.Component {
         return (
             <>
                 <div className={"donations-container"}>
-                    <div className={"donations-background"}></div>
-                    <div className={"donations-main"}>
-                        <p className={"donations-main-header"}>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</p>
-                        <div className={"home-header-line"}></div>
-                        <p className={"donations-header-steps"}>Wystarczą 4 proste kroki:</p>
-                        <div className={"donations-steps"}>
-                            <RotatedSquare stepNumber={1} text={"Wybierz rzeczy"}/>
-                            <RotatedSquare stepNumber={2} text={"Spakuj je w worki"}/>
-                            <RotatedSquare stepNumber={3} text={"Wybierz fundację"}/>
-                            <RotatedSquare stepNumber={4} text={"Zamów kuriera"}/>
+                    <div className={"donations-header-container"}>
+                        <div className={"donations-background"}></div>
+                        <div className={"donations-main"}>
+                            <p className={"donations-main-header"}>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</p>
+                            <div className={"home-header-line"}></div>
+                            <p className={"donations-header-steps"}>Wystarczą 4 proste kroki:</p>
+                            <div className={"donations-steps"}>
+                                <RotatedSquare stepNumber={1} text={"Wybierz rzeczy"}/>
+                                <RotatedSquare stepNumber={2} text={"Spakuj je w worki"}/>
+                                <RotatedSquare stepNumber={3} text={"Wybierz fundację"}/>
+                                <RotatedSquare stepNumber={4} text={"Zamów kuriera"}/>
+                            </div>
                         </div>
                     </div>
+                    <DonationsStepArea/>
                 </div>
             </>
         )
@@ -33,8 +38,48 @@ class RotatedSquare extends React.Component {
                         <p className={"donations-rotated-square-text"}>{this.props.text}</p>
                     </div>
                 </div>
+
             </>
         );
+    }
+}
+class DonationsStepArea extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            step: 1
+        }
+    }
+    render() {
+        return(
+            <>
+                <div className={"steps-area"}>
+                    {this.state.step !== 5 ? <div className={"steps-header"}>
+                        {this.state.step === 1 ? <DonationsStepHeader text={"Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać."}/> : null}
+                        {this.state.step === 2 ? <DonationsStepHeader text={"Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ"}/> : null}
+                        {this.state.step === 3 ? <DonationsStepHeader text={"Jeśli wiesz komu chcesz pomóc, możesz wpisać nazwę tej organizacji w wyszukiwarce. Możesz też filtrować organizacje po ich lokalizacji bądź celu ich pomocy."}/> : null}
+                        {this.state.step === 4 ? <DonationsStepHeader text={"Podaj adres oraz termin odbioru rzeczy."}/> : null }
+                    </div> : null}
+                    <p className={"step-counter"}>{this.state.step}/4</p>
+                    <nav>
+                        {this.state.step !== 1 ? <button onClick={()=>(this.setState({step: this.state.step-1}))} className={"step-button"}>Wstecz</button> : null}
+                        {this.state.step !== 5 ? <button onClick={()=>(this.setState({step: this.state.step+1}))} className={"step-button"}>Dalej</button> : <button className={"step-button"}>Potwierdzam</button>}
+                    </nav>
+                </div>
+
+
+            </>
+        )
+    }
+}
+class DonationsStepHeader extends React.Component {
+    render() {
+        return(
+            <>
+                <h1 className={"step-h1"}>Ważne!</h1>
+                <p>{this.props.text}</p>
+            </>
+        )
     }
 }
 export default Donations
