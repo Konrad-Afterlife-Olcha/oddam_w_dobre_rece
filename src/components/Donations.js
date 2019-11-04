ddbae89a843ac04e1814ffba4421803a92af2c46
Konrad <olchakonrad@gmail.com>
2019-11-04 15:02:22 +0100

    continuation with styling form

--- src/components/Donations.js
+++ src/components/Donations.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { store } from 'react-easy-state'
 const donationInfo = store({
     step1: "ubrania w dobrym stanie",
-    step1option: "option1"
+    step1option: "option1",
+    step2: "1",
+    step3location: "",
+    step3helpGroups: []
 });
 
 class Donations extends React.Component {
@@ -50,7 +53,7 @@ class DonationsStepArea extends React.Component {
     constructor(){
         super();
         this.state = {
-            step: 1
+            step: 4
         }
     }
     render() {
@@ -111,51 +114,57 @@ class StepOne extends React.Component {
         return(
             <>
                 <div className={"step-details"}>
+                    <p className={"step-header"}>Zaznacz co chcesz oddać:</p>
                     <form>
                         <input
+                            className={"radio1"}
                             type="radio"
                             value={"option1"}
                             checked={this.state.selectedOption === "option1"}
                             onChange={this.handleOptionChange}
                             name={"ubrania w dobrym stanie"}
                         />
-                        ubrania, które nadają się do ponownego użycia
+                        <span className={"radioText"}>ubrania, które nadają się do ponownego użycia</span>
                         <br/>
                         <input
+                            className={"radio1"}
                             type="radio"
                             value={"option2"}
                             checked={this.state.selectedOption === "option2"}
                             onChange={this.handleOptionChange}
                             name={"ubrania do wyrzucenia"}
                         />
-                        ubrania, do wyrzucenia
+                        <span className={"radioText"}>ubrania, do wyrzucenia</span>
                         <br/>
                         <input
+                            className={"radio1"}
                             type="radio"
                             value={"option3"}
                             checked={this.state.selectedOption === "option3"}
                             onChange={this.handleOptionChange}
                             name={"zabawki"}
                         />
-                        zabawki
+                        <span className={"radioText"}>zabawki</span>
                         <br/>
                         <input
+                            className={"radio1"}
                             type="radio"
                             value={"option4"}
                             checked={this.state.selectedOption === "option4"}
                             onChange={this.handleOptionChange}
                             name={"książki"}
                         />
-                        książki
+                        <span className={"radioText"}>książki</span>
                         <br/>
                         <input
+                            className={"radio1"}
                             type="radio"
                             value={"option5"}
                             checked={this.state.selectedOption === "option5"}
                             onChange={this.handleOptionChange}
                             name={"inne"}
                         />
-                        inne
+                        <span className={"radioText"}>inne</span>
 
                     </form>
                 </div>
@@ -168,17 +177,111 @@ class StepTwo extends React.Component {
         return(
             <>
                 <div className={"step-details"}>
-                    {donationInfo.step1}
+                    <p className={"step-header"}>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</p>
+                    <form>
+                        <span>Liczba 60l worków: </span>
+                        <select name="bags" id="bags" onChange={(e)=>(donationInfo.step2 = e.target.value)}>
+                            <option value="" disabled defaultValue>---wybierz---</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+
+                        </select>
+                    </form>
                 </div>
             </>
         )
     }
 }
 class StepThree extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            optionsSelected: donationInfo.step3helpGroups,
+            localizationSpecific: "",
+            locationSelectDisabled: false
+        }
+    }
+    handleHelpGroupChange = event =>{
+        if(!this.state.optionsSelected.includes(event.target.value)){
+            this.setState({
+                optionsSelected: [...this.state.optionsSelected , event.target.value]
+            });
+            donationInfo.step3helpGroups.push(event.target.value)
+        } else {
+            this.setState({
+                optionsSelected: this.state.optionsSelected.filter((el)=>{
+                    return el !==event.target.value
+                })
+            })
+
+            donationInfo.step3helpGroups = donationInfo.step3helpGroups.filter((el)=>{
+                return el !== event.target.value
+            })
+        }
+
+    }
     render() {
         return(
             <>
-                <div className={"step-details"}></div>
+                <div className={"step-details"}>
+                    <form>
+                        <p className={"step-header"}>Lokalizacja:</p>
+                        <select className={"select-location"} name="location" id="location" onChange={(e)=>(donationInfo.step3location = e.target.value)} disabled={this.state.locationSelectDisabled}>
+                            <option defaultValue>---wybierz---</option>
+                            <option value="Poznań">Poznań</option>
+                            <option value="Warszawa">Warszawa</option>
+                            <option value="Kraków">Kraków</option>
+                            <option value="Wrocław">Wrocław</option>
+                            <option value="Katowice">Katowice</option>
+                        </select>
+                        <p className={"step-small-header"}>Komu chcesz pomóc?</p>
+                        <input
+                            type="checkbox"
+                            value={"dzieciom"}
+                            onChange={this.handleHelpGroupChange}
+                            checked={this.state.optionsSelected.includes("dzieciom")}
+                        />dzieciom
+                        <input
+                            type="checkbox"
+                            value={"samotnym matkom"}
+                            onChange={this.handleHelpGroupChange}
+                            checked={this.state.optionsSelected.includes("samotnym matkom")}
+                        />samotnym matkom
+                        <input
+                            type="checkbox"
+                            value={"bezdomnym"}
+                            onChange={this.handleHelpGroupChange}
+                            checked={this.state.optionsSelected.includes("bezdomnym")}
+                        />bezdomnym
+                        <input
+                            type="checkbox"
+                            value={"niepełnosprawnym"}
+                            onChange={this.handleHelpGroupChange}
+                            checked={this.state.optionsSelected.includes("niepełnosprawnym")}
+                        />niepełnosprawnym
+                        <input
+                            type="checkbox"
+                            value={"osobom starszym"}
+                            onChange={this.handleHelpGroupChange}
+                            checked={this.state.optionsSelected.includes("osobom starszym")}
+                        />osobom starszym
+                        <p className={"step-small-header"}>Wpisz nazwę konkretnej lokalizacji(opcjonalnie)</p>
+                        <input
+                            type="text"
+                            value={donationInfo.step3location}
+                            onChange={(e)=>{
+                                this.setState({
+                                    localizationSpecific: e.target.value,
+                                    locationSelectDisabled: e.target.value !== ""
+                                });
+                                donationInfo.step3location = e.target.value
+                            }}
+                        />
+                    </form>
+                </div>
             </>
         )
     }
@@ -187,7 +290,13 @@ class StepFour extends React.Component {
     render() {
         return(
             <>
-                <div className={"step-details"}></div>
+                <div className={"step-details"}>
+                   <p className={"step-header"}>Podaj adres oraz termin odbioru rzeczy przez kuriera</p>
+                    <form className={"address-form"}>
+                        <div className={"address-info"}></div>
+                        <div className={"data-info"}></div>
+                    </form>
+                </div>
             </>
         )
     }
