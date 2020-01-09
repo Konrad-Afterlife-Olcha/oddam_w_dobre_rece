25492fdbbfc404f4200698ac2e15e96202a1f3d2
Konrad <olchakonrad@gmail.com>
2020-01-09 18:07:46 +0100

    responsive About Us section

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,24 +1,19 @@
 import React from 'react';
-
+import Signature from "./Icons/Signature"
 
 class HomeAboutUs extends React.Component {
     render() {
-
         return (
             <>
-                <div className={"home-aboutus-container"}>
-                    <div className={"home-aboutus-article-container"}>
-                        <div className={"home-aboutus-article"}>
-
-                            <h1 className={"home-aboutus-header"}>O nas</h1>
-                            <div className={"home-header-line"}></div>
-                            <p className={"home-aboutus-text"}>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                            <div className={"home-aboutus-signature"}></div>
-                        </div>
-                    </div>
-                    <div className={"home-aboutus-picture"}></div>
+                <div className={"aboutus__container"}>
+                    <article className={"aboutus__article"}>
+                        <h1 className={"aboutus__title"}>O nas</h1>
+                        <div className={"line"}></div>
+                        <p className={"aboutus__text"}>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                        <Signature className={"aboutus__signature"} />
+                    </article>
+                    <div className={"aboutus__picture"}></div>
                 </div>
-
             </>
         )
     }
